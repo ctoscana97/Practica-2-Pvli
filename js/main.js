8ca96674b6524c9015e4a7594d2bbf54dc63ed95
Mario <mjcguada@gmail.com>
2016-11-27 23:13:41 +0100

    Ejercicio 1 avanzado

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ function prettifyEffect(obj) {
         return `${sign}${obj[key]} ${key}`;
     }).join(', ');
 }
-
-
+    /*
+    Apuntes: variable global document
+        desde javascript modificamos el document
+    */
+//document.querySelector(."examples").styles.backgroundColor = "red";
+//Styles.display="block"; para mostrar
 battle.setup({
     heroes: {
         members: [
@@ -39,8 +43,55 @@ battle.on('turn', function (data) {
     console.log('TURN', data);
 
     // TODO: render the characters
+
+    //var personajes = document.querySelector(#heroes);
+    //var tituloHeroes = document.getElementById("heroes");
+    //tituloHeroes.lastChild.nodeValue =  prettifyEffect(RPG.entities.characters.heroTank);
+    //tituloHeroes.style.color = "red";
+    var heroes = [
+    {name: 'Tank', hp: 80, maxHp: 80, mp: 30, maxMp: 30, id: 'Tank'},
+    {name: 'Wizz', hp: 40, maxHp: 40, mp: 100, maxMp: 100, id: 'Wizz'}
+    ];
+
+    var monsters = [
+    {name: 'Slime', hp: 40, maxHp: 40, mp: 50, maxMp: 50, id: 'Slime'},
+    {name: 'Bat', hp: 5, maxHp: 5, mp: 0, maxMp: 0, id: 'Bat'},
+    {name: 'Skeleton', hp: 100, maxHp: 100, mp: 0, maxMp: 0, id: 'Tank'},
+    {name: 'Bat', hp: 5, maxHp: 5, mp: 0, maxMp: 0, id: 'Bat 2'}
+    ];
+
+    var listH = document.getElementById('heroes');
+    heroes.forEach(function (character) {
+        var li = document.createElement('li');
+        li.innerHTML = character.id + ' (HP: <strong>' + character.hp + '</strong>/'+ 
+        character.maxHp +', MP: <strong>' + character.mp + '</strong>/'+ character.maxMp + ')';
+        listH.appendChild(li);
+    });
+
+    var listM = document.getElementById('monsters');
+    monsters.forEach(function (character) {
+        var li = document.createElement('li');
+        li.innerHTML = character.id + ' (HP: <strong>' + character.hp + '</strong>/'+ 
+        character.maxHp +', MP: <strong>' + character.mp + '</strong>/'+ character.maxMp + ')';
+        listM.appendChild(li);
+    });
+    
+
+    var lista = document.getElementsByTagName("");
+    lista.style.color = "red";
+    personajesH.getElementById("tank").innerHTML = "hola";
+    //element.setAttribute(attribute, value)
+    var hp = personajesH.getAttribute("tank"); // 
+    hp.nodeValue = 80; // 
+    personajesH.setAttribute("tank", 80);
+
+    var tank = document.querySelector("li.data-chara-id[name=mojon]");
+
+    //document.getElementById('').innerHTML = ;
+    
     // TODO: highlight current character
     // TODO: show battle actions form
+    //obj.setActive()
 });
 
 battle.on('info', function (data) {
@@ -56,11 +107,12 @@ battle.on('end', function (data) {
     // TODO: display 'end of battle' message, showing who won
 });
 
-window.onload = function () {
-    actionForm = document.querySelector('form[name=select-action]');
-    targetForm = document.querySelector('form[name=select-target]');
-    spellForm = document.querySelector('form[name=select-spell]');
-    infoPanel = document.querySelector('#battle-info');
+window.onload = function () {   //LLamada una vez cargados todos los recursos
+    actionForm = document.querySelector('form[name=select-action]'); //form, tipo de etiqueta [atributos = valor] //busqueda unica
+    targetForm = document.querySelector('form[name=select-target]'); //En vez de name ponemos el style (para el estilo claro)
+    spellForm = document.querySelector('form[name=select-spell]');  //buscar por ID es util (son unicos)
+    infoPanel = document.querySelector('#battle-info'); //# es ID
+    //buscar por conjunto = buscar por query
 
     actionForm.addEventListener('submit', function (evt) {
         evt.preventDefault();
